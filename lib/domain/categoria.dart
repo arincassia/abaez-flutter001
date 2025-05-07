d608ea0c59e71424b86ab4853e39ad28e6109acb
Chucky2010 <sebas10ibarra81@gmail.com>
2025-05-07 10:15:17 -0300

    Se implemento la serializacion de la clase Categoria

--- lib/domain/categoria.dart
+++ lib/domain/categoria.dart
@@ -1,5 +1,10 @@
+import 'package:json_annotation/json_annotation.dart'; 
+part 'categoria.g.dart'; 
+
+@JsonSerializable()
 // category.dart
 class Categoria {
+  @JsonKey(name: '_id', includeToJson: false)
   final String id;
   final String nombre; 
   final String descripcion; 
@@ -12,22 +17,26 @@ class Categoria {
     this.imagenUrl,
   });
 
-  // Método para convertir un JSON de la API a un objeto Category
-  factory Categoria.fromJson(Map<String, dynamic> json) {
-    return Categoria(
-      id: json['_id'] as String, // El ID lo asigna CrudCrud
-      nombre: json['nombre'] as String,
-      descripcion: json['descripcion'] as String,
-      imagenUrl: json['imagenUrl'] as String?,
-    );
-  }
+  // // Método para convertir un JSON de la API a un objeto Category
+  // factory Categoria.fromJson(Map<String, dynamic> json) {
+  //   return Categoria(
+  //     id: json['_id'] as String, // El ID lo asigna CrudCrud
+  //     nombre: json['nombre'] as String,
+  //     descripcion: json['descripcion'] as String,
+  //     imagenUrl: json['imagenUrl'] as String?,
+  //   );
+  // }
 
-  // Método para convertir el objeto Category a JSON para enviar a la API
-  Map<String, dynamic> toJson() {
-    return {
-      'nombre': nombre,
-      'descripcion': descripcion,
-      'imagenUrl': imagenUrl,
-    };
-  }
+  // // Método para convertir el objeto Category a JSON para enviar a la API
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'nombre': nombre,
+  //     'descripcion': descripcion,
+  //     'imagenUrl': imagenUrl,
+  //   };
+  // }
+
+  factory Categoria.fromJson(Map<String, dynamic> json) => _$CategoriaFromJson(json); 
+  
+  Map<String, dynamic> toJson() => _$CategoriaToJson(this);
 }
